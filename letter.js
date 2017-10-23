79d05687f9478ccf5951754d39f8b8cba25f7ae9
blaws006 <blaws006>
2017-10-22 22:52:25 -0400

    Test

--- letter.js
+++ letter.js
@@ -1,3 +1,4 @@
+//Here, we're bringing in the words for the word.js file. Will create more words when I figure out how to make the first word work...
 var words = require("./word");
 
 console.log(words.word);
