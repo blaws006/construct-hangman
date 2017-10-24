3b7cb129cd524f8568da235029d229690c8d55e6
blaws006 <blaws006>
2017-10-23 23:53:28 -0400

    Letters logged and replaced by _

--- letter.js
+++ letter.js
@@ -1,4 +1,16 @@
 //Here, we're bringing in the words for the word.js file. Will create more words when I figure out how to make the first word work...
 var words = require("./word");
 
-console.log(words.word);
+function Letter(letters) {
+    this.letters = letters;
+}
+
+Letter.prototype.replaceLetter = function () {
+    var hidden = this.letters.replace(/[A-Z]/gi,  " _ ");
+  
+console.log(hidden);
+};
+
+var underscore = new Letter(words.word);
+
+underscore.replaceLetter();
